406c37fbf74b11be6866994cbca7fc465df54c33
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-23 17:33:17 +0900

    배열 규칙

---
+++ Exam06.java
@@ -0,0 +1,24 @@
+
+public class Exam06 {
+
+	public static void main(String[] args) {
+		
+		int[] array1 = {10,20,30,40,50};
+		int[] array2 = array1;
+		
+		array1[3] = 999; 
+		
+		
+		System.out.println(array2[3]); // 주소가 복사되었기 때문에 바뀐 값으로 출력한다.
+		
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
