3ffb68a0e11c1f87c8aafcb067a0a4d4003f0107
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-23 17:33:37 +0900

    배열 실습

---
+++ Exam07.java
@@ -0,0 +1,52 @@
+
+public class Exam07 {
+
+	public static void main(String[] args) {
+		
+		// 길이가 5인 배열을 만들고 1,2,3,4,5 값을 각 인덱스 0번째 부터 저장한 다음 배열안에 모든 값을 출력하시오
+		
+		int[]array = {1,2,3,4,5};
+//		System.out.println(array[0]);
+//		System.out.println(array[1]);
+//		System.out.println(array[2]);
+//		System.out.println(array[3]);
+//		System.out.println(array[4]);
+		
+		int[]array1 = new int[10];
+		for(int i = 0; i < array1.length; i++) {
+			array1[i] = i+1; //인덱스에 값을 부여.
+			System.out.println(array1[i]);
+		}
+		
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]); // 길이를 알고있을때 사용가능.
+		}
+		
+//		int[] array1 = new int[5];
+//		array1[0] = 1;
+//		array1[1] = 2;
+//		array1[2] = 3;
+//		array1[3] = 4;
+//		array1[4] = 5;
+		
+		int[]array2;
+		array2 = new int[5];
+		
+		int array3[] = new int[5];
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
